3867ba65184080c3caeb4618baa8d54990fbbe70
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-05-04 22:47:22 +0800

    adding extra words once initialized from wordapi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-window.addEventListener('load', init);
-
+window.addEventListener("load", init);
 
 // Globals
 
@@ -18,81 +17,92 @@ let score = 0;
 let highScore = 0;
 let isPlaying;
 
-
 // Dom Elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const highScoreDisplay = document.querySelector('#high-score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const highScoreDisplay = document.querySelector("#high-score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
 
-
-const words = [
-    'hat',
-    'time',
-    'person',
-    'year',
-    'way',
-    'day',
-    'thing',
-    'man',
-    'world',
-    'life',
-    'hand',
-    'part',
-    'child',
-    'eye',
-    'woman',
-    'place',
-    'work',
-    'week',
-    'case',
-    'point',
-    'company',
-    'number',
-    'good',
-    'first',
-    'new',
-    'last',
-    'long',
-    'great',
-    'little',
-    'own',
-    'other',
-    'old',
-    'right',
-    'big',
-    'high',
-    'different',
-    'small',
-    'large',
-    'next',
-    'early',
-    'young',
-    'important',
-    'few',
-    'public',
-    'bad',
-    'same',
-    'able',
+let words = [
+    "hat",
+    "time",
+    "person",
+    "year",
+    "way",
+    "day",
+    "thing",
+    "man",
+    "world",
+    "life",
+    "hand",
+    "part",
+    "child",
+    "eye",
+    "woman",
+    "place",
+    "work",
+    "week",
+    "case",
+    "point",
+    "company",
+    "number",
+    "good",
+    "first",
+    "new",
+    "last",
+    "long",
+    "great",
+    "little",
+    "own",
+    "other",
+    "old",
+    "right",
+    "big",
+    "high",
+    "different",
+    "small",
+    "large",
+    "next",
+    "early",
+    "young",
+    "important",
+    "few",
+    "public",
+    "bad",
+    "same",
+    "able"
 ];
 
 // Initialize Game
 function init() {
+    // adding extra words to the words array from wordapi
+    httpGet("https://mywordapi.herokuapp.com/api/100", (res) => {
+        const data = JSON.parse(res);
+        words = words.concat(data.words);
+    });
     // show number of seconds in UI
     seconds.innerHTML = currentLevel;
     // load word from array
     showWord(words);
     // Start matching on word input
-    wordInput.addEventListener('input', startMatch);
+    wordInput.addEventListener("input", startMatch);
     // Call countdown every second
     setInterval(countdown, 1000);
     // Check game status
     setInterval(checkStatus, 50);
 }
 
+// Handle HTTP request in the application
+const httpGet = (url, callback, err = console.error) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.onload = () => callback(request.responseText);
+    request.onerror = () => err(request);
+    request.send();
+};
 
 // Start match
 function startMatch() {
@@ -100,7 +110,7 @@ function startMatch() {
         isPlaying = true;
         time = currentLevel + 1;
         showWord(words);
-        wordInput.value = '';
+        wordInput.value = "";
         score++;
     }
 
@@ -110,17 +120,15 @@ function startMatch() {
     } else {
         scoreDisplay.innerHTML = score;
     }
-
 }
-
 
 // Match currentWord to wordInput
 function matchWords() {
     if (wordInput.value === currentWord.innerHTML) {
-        message.innerHTML = 'Correct!!!';
+        message.innerHTML = "Correct!!!";
         return true;
     } else {
-        message.innerHTML = 'typing...';
+        message.innerHTML = "typing...";
         return false;
     }
 }
@@ -132,7 +140,6 @@ function showWord(words) {
     // Output random word
     currentWord.innerHTML = words[randIndex];
 }
-
 
 // Countdown timer
 function countdown() {
@@ -148,11 +155,10 @@ function countdown() {
     timeDisplay.innerHTML = time;
 }
 
-
 // Check game status
 function checkStatus() {
     if (!isPlaying && time === 0) {
-        message.innerHTML = 'Game Over!!!';
+        message.innerHTML = "Game Over!!!";
         if (score > highScore) {
             highScore = score;
         }
